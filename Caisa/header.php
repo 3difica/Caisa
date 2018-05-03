c85b5ded8066ae3893d168ebdfa71e83878fc74b
Daniel <daniel@3difica.com>
2018-05-03 18:30:36 -0500

    bvv

---
+++ Caisa/header.php
@@ -0,0 +1,56 @@
+
+<div   id="info" class="container-fluid d-flex justify-content-center header_colorgris pl-0 pr-0 d-none"  >
+  <div  class=" col-lg-12 contenedor1024  header_colorgris  ">
+    <div class=" row  d-flex justify-content-end textblanco  pt-3 pr-0">
+       <p><i class="fa fa-phone pr-2 " ></i>¡Llámanos! <strong>01 800 63 22472</strong></p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <p > ¡Síguenos! <img src="img/iconos/facebook.png" class="img-fluid pl-1 pr-2 "><img src="img/iconos/youtube.png" class="img-fluid"></p>
+    </div>
+  </div>
+</div>
+
+
+
+<div class="container-fluid d-flex justify-content-center pl-0 pr-0 d-none ">
+    <div class="col-md-12  contenedor1024   pl-0 pr-0 " >
+
+  <nav  id="navquitar"  class="pl-0 pr-0 d-none">
+
+    <ul class="nav nav-pills">
+    <div class="col-lg-2 col-md-2 col-ms-2 pl-0 pr-0 pt-3">
+    <li class="nav-item pl-0 pr-0">
+       <a class="nav-link st_nav-link dropdown-toggle anim-underline pl-0 pr-0" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">CAISA
+      </a>
+        <div class="dropdown-menu st_dropdown-menu" href="#"aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item st_dropdown-item anim-underline menurosa " href="quienes_somos.php">QUIENES SOMOS</a>
+          <a class="dropdown-item st_dropdown-item anim-underline menurosa " href="llave_sueños.php">LA LLAVE DE TUS SUEÑOS</a>
+          <a class="dropdown-item st_dropdown-item anim-underline menurosa" href="gente_caisa.php">GENTE CAISA</a>
+          <a class="dropdown-item st_dropdown-item anim-underline menurosa " href="inversionistas.php">INVERSIONISTAS</a>
+         <!--  <a class="dropdown-item st_dropdown-item anim-underline menurosa" href="">RENTA</a> -->
+          <a class="dropdown-item st_dropdown-item anim-underline menurosa " href="atencion_clientes.php">ATENCIÓN A CLIENTES</a>
+        </div>
+    </li>
+  </div>
+  <div class="col-lg-2 col-md-2 col-ms-2 pl-0 pr-0 pt-3">
+    <li class="nav-item pl-0 pr-0">
+      <a class="nav-link pl-0 pr-0" href="desarrollo.php">DESARROLLO</a>
+    </li>
+  </div>
+  <div class="col-lg-4 col-md-4 col-ms-4  pl-0 pr-0 "  align="center">
+    <li class="nav-item pl-0 pr-0">
+      <a class="nav-link st_nav-link pl-0 pr-0" id="logo_san_angel" href="index.php"><img src="img/logocaisa.png" class="img-fluid logo"  ></a>
+    </li>
+  </div>
+  <div class="col-lg-2 col-md-2 col-ms-2 pl-0 pr-0 pt-3 d-flex justify-content-end">
+    <li class="nav-item pl-0 pr-0">
+      <a class="nav-link pl-0 pr-0 " href="#"><img src="img/iconos/calendario.png" class="img-fluid" > AGENDAR CITA</a>
+    </li>
+  </div>
+  <div class="col-lg-2 col-md-2 col-ms-2 pl-0 pr-0 pt-3 d-flex justify-content-end">
+    <li class="nav-item pl-0 pr-0">
+      <a class="nav-link pl-0 pr-0" href="#"><img src="img/iconos/usuario.png" class="img-fluid" > CONDÓMINOS</a>
+    </li>
+  </div>
+  </ul>
+  </nav>
+
+  </div>
+</div>
---
+++ Caisa/js/script.js
@@ -0,0 +1,56 @@
+var Clase = function(){
+  
+}
+Clase.prototype.variable = "uno";;
+Clase.prototype.grandesCosas = function(){
+
+};
+
+var intancia = new Clase("hola mundo");
+
+inntancia.grandesCosas();
+
+(function(){
+
+	alert("function anonima");
+
+	//modulo auto ejecutable
+
+	var modulo = (function(){
+		alert("modulo")
+
+
+	})();
+
+
+   var funcionDeclarada = function(){
+   	return une;
+
+   }
+
+   function nombrada(){
+   	return false;
+   }
+
+})();
+
+
+
+(function(){
+
+	alert("Es hora de hacer grandes Cosas");
+	
+});
+
+
+
+(function(){
+
+	alert("Ahora?");
+	return 0;
+	
+});
+
+
+
+
